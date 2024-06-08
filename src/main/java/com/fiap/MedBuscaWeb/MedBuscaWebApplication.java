129eba4e17aa18831adea2d857587f8c385f0f70
Ana Sá <anasadevweb@gmail.com>
2024-06-08 16:14:17 +0200

    Criação do projeto

---
+++ src/main/java/com/fiap/MedBuscaWeb/MedBuscaWebApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.MedBuscaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBuscaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedBuscaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/MedBuscaWeb/MedBuscaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.MedBuscaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBuscaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
